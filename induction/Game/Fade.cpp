db76b10c5638a11ec0176d494106d4a15d7dd4e6
kbc18b01 <kbc18b01@stu.kawahara.ac.jp>
2019-02-14 14:22:53 +0900

    fade

--- induction/Game/Fade.cpp
+++ induction/Game/Fade.cpp
@@ -13,10 +13,16 @@ Fade::~Fade()
 
 bool Fade::Start()
 {
+	m_texture.CreateFromDDSTextureFromFile(L"sprite/fade.dds");
+	m_sprite.Init(m_texture, GraphicsEngine().Get2DSpaceScreenWidth, GraphicsEngine().Get2DSpaceScreenHeight);
 	return true;
 }
 
 void Fade::Update()
+{
+
+}
+void Fade::PostRender(CRenderContext& rc)
 {
 
 }
