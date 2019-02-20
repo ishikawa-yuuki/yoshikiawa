43934349a8791e5f73ecaa823312dbc96861975b
kbc18b01 <kbc18b01@stu.kawahara.ac.jp>
2019-02-20 22:28:21 +0900

    ステージ選択の調整をしました。

--- induction/Game/SSPlayer.cpp
+++ induction/Game/SSPlayer.cpp
@@ -19,15 +19,17 @@ bool SSPlayer::Start()
 	m_effect = NewGO<prefab::CEffect>(0);
 	m_effect->Play(L"effect/hikari.efk");
 	m_effect->SetScale({ 30.0f,30.0f,30.0f });
+	m_sspoint = m_SS->m_ssPoint[0];
 	return true;
 }
 
 void SSPlayer::Update()
 {
-	if (m_State == true) {
+	
+	if (m_AfterState == true) {
 		MoveState();
 	}
-	else {
+	 if(m_State == false){
 		Move();
 	}
 	m_position.y = 50;
@@ -36,8 +38,10 @@ void SSPlayer::Update()
 
 void SSPlayer::Move()
 {
- 	const float pl_speed = 1200.0f;//Œõ‚Ì‘¬“x 
+
+	
 	if (Pad(0).IsTrigger(enButtonRight)) {
+		m_AfterState = true;
 		m_State = true;
 		m_SelectOK = false;
 		switch (m_stage) {
@@ -52,7 +56,9 @@ void SSPlayer::Move()
 		}
 	}
 	else if (Pad(0).IsTrigger(enButtonLeft)) {
+		m_AfterState = true;
 		m_State = true;
+		m_SelectOK = false;
 		switch (m_stage) {
 		case stage3:
 			m_sspoint = m_SS->m_ssPoint[1];
@@ -67,18 +73,22 @@ void SSPlayer::Move()
 }
 void SSPlayer::MoveState()
 {
-	CVector3 v = m_position - m_sspoint->m_position;
-	if (v.LengthSq() <= 60.0f * 60.0f) {
-		m_State = false;
-		m_SelectOK = true;
+	m_v = m_position - m_sspoint->m_position;
+	if (m_v.LengthSq() <= 60.0f * 60.0f) {
 		m_moveSpeed = CVector3::Zero;
+		m_AfterState = false;
+	}
+    if (m_v.LengthSq() <= 250.0f * 250.0f) {
+			m_State = false;
+			m_SelectOK = true;
+			
 	}
 	else {
-		m_moveSpeed = m_sspoint->m_position - m_position;
-		m_moveSpeed.y = 0.0f;
-		m_moveSpeed.Normalize();
-		m_moveSpeed *= v.LengthSq() / (350.0f * 350.0f) * 12.0;
-		
+			m_moveSpeed = m_sspoint->m_position - m_position;
+			m_moveSpeed.y = 0.0f;
+			m_moveSpeed.Normalize();
+			m_moveSpeed *= m_v.LengthSq() / (350.0f * 350.0f) * 12.0;
 	}
 	m_position += m_moveSpeed;
+	
 }
