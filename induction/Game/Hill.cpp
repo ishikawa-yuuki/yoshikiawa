0fc023a75956387011cbf6cb19ad2794cc389a62
Satoshi Yoshinaga <kbc18b31@stu.kawahara.ac.jp>
2019-05-07 18:25:23 +0900

    ヒルのクールダウン修正

--- induction/Game/Hill.cpp
+++ induction/Game/Hill.cpp
@@ -35,12 +35,12 @@ void Hill::Update()
 	CVector3 diff_h = m_human->GetPosition() - m_position;
 	diff_h.y = 0.0f;
 	if (m_timer < 60) {
-		m_timer += 60 * GameTime().GetFrameDeltaTime();
+		m_timer += 20 * GameTime().GetFrameDeltaTime();
 		diff_h.Normalize();
 		m_position += diff_h * 50.0f * GameTime().GetFrameDeltaTime();
 	}
 	else {
-		m_timer += 60 * GameTime().GetFrameDeltaTime();
+		m_timer += 20 * GameTime().GetFrameDeltaTime();
 		m_position = m_position;
 		if (m_timer > 100) {
 			m_timer = 0;
