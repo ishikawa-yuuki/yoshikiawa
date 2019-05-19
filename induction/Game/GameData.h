e64f10e03b598e2ce9c97d1f7d25ab67ea830dad
komura athushi <kbc18b09@kawahara.ac.jp>
2019-05-19 15:13:41 +0900

    Hill

--- induction/Game/GameData.h
+++ induction/Game/GameData.h
@@ -15,7 +15,7 @@ public:
 		enState_Stage2,
 		enState_Stage3,
 		enState_StageFirst = enState_Stage1,
-		enState_StageLast = enState_Stage2
+		enState_StageLast = enState_Stage3
 	};
 	//ステージをクリアした！
 	void SetStageClear() 
@@ -64,7 +64,7 @@ public:
 	}
 	static const int m_stagelimit = 3;								//ステージの数
 public:
-	bool m_stageclear[m_stagelimit] = { false,false,false };		//各ステージのクリア状況
+	bool m_stageclear[m_stagelimit] = { true,true,false };			//各ステージのクリア状況
 	bool m_stagecheck[m_stagelimit] = { false,false,false };		//チェックポイントの通過状況
 	StageNumber m_stagenumber = enState_Stage1;						//プレイヤーが選択したステージの番号
 	const char* m_effectname = "Effect";							//ステージ終了時に強制的に消去したいエフェクトに名前を付けるときに使う
--- induction/Game/Hill.h
+++ induction/Game/Hill.h
@@ -9,6 +9,10 @@ public:
 	~Hill();
 	bool Start();
 	void Update();
+	void Move();
+	void Stop();
+	void Turn();
+	void Kill();
 	void SetPosition(const CVector3& pos)
 	{
 		m_position = pos;
@@ -24,14 +28,21 @@ public:
 private:
 	prefab::CSkinModelRender* m_skinModelRender = nullptr;
 	CVector3 m_position = CVector3::Zero;
+	CVector3 m_movespeed = CVector3::Zero;
+	CVector3 m_parallel = CVector3::Zero;
 	CVector3 m_scale = CVector3::One;
 	CQuaternion m_rotation = CQuaternion::Identity;
 	enum HillAnimationClip {
 		enAnimationClip_move,
 		enAnimationClip_num
 	};
+	enum State {
+		enState_Move,
+		enState_Stop
+	};
+	State m_state = enState_Stop;
 	CAnimationClip m_animeClip[enAnimationClip_num];
 	int m_timer = 0;
-	Human* m_human;
+	Human* m_human = nullptr;
 };
 
