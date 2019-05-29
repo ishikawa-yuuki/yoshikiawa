5ef4a400ec226283ff35a048c7c952893c94435b
Satoshi Yoshinaga <kbc18b31@stu.kawahara.ac.jp>
2019-05-29 20:56:46 +0900

    ゲームデータを1ステージめだけ遊べるようにした

--- induction/Game/GameData.h
+++ induction/Game/GameData.h
@@ -87,8 +87,8 @@ public:
 	}
 	static const int m_stagelimit = 5;								//ステージの数
 public:
-	bool m_stageclear[m_stagelimit] = { true,true,true,true,true };			//各ステージのクリア状況
-	bool m_stagecheck[m_stagelimit] = { true,true,true,true,true };		//チェックポイントの通過状況
+	bool m_stageclear[m_stagelimit] = { false,false,false,false,false };			//各ステージのクリア状況
+	bool m_stagecheck[m_stagelimit] = { false,false,false,false,false };		//チェックポイントの通過状況
 	std::vector<CameraDegree> m_cameradegreelist;									//各チェックポイントのカメラの角度
 	StageNumber m_stagenumber = enState_Stage1;						//プレイヤーが選択したステージの番号
 	const char* m_effectname = "Effect";							//ステージ終了時に強制的に消去したいエフェクトに名前を付けるときに使う
